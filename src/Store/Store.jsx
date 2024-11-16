dad025af265c3b1ae26d332846555009e8760256
Blaz <165042109+BlazKzz@users.noreply.github.com>
2024-11-16 04:44:04 -0300

    Update Store.jsx

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,6 +1,6 @@
 import { create } from 'zustand'; // función que se utilizará para crear el Store
 
-const Store = create((set) => ({ // se importa create es la función utilizada para crear el store
+export default create((set) => ({ // se importa create es la función utilizada para crear el store
     cart: [], // Se define el estado inicial del Store que en este caso es un array vacío
     // Se definen dos funciones
     addToCart: (product) => set((state) => ({ // Esta  función toma product como argumento y lo agrega al carrito, utiliza 'set' para actualizar el estado
@@ -10,5 +10,3 @@ const Store = create((set) => ({ // se importa create es la función utilizada p
         cart: state.cart.filter((item) => item.id !== id), // utiliza 'filter' para crear un nuevo array que excluye el producto con el Id especificado 
     })),
 }));
-
-export default Store;
